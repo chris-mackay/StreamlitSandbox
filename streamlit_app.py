96cc835e7f25522c6f0571bf7460fc0772ee168c
Christopher Mackay <cmackay732@gmail.com>
2025-06-11 21:43:17 -0400

    Added more controls

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from io import StringIO
+from datetime import *
 
 genre = st.radio(
     "What's your favorite movie genre",
@@ -96,3 +97,27 @@ option = st.selectbox(
 )
 
 st.write("You selected:", option)
+
+on = st.toggle("Activate feature")
+
+if on:
+    st.write("Feature activated")
+
+age = st.slider("How old are you?", 0, 130, 25)
+st.write("I'm ", age, "years old")
+
+values = st.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
+st.write("Values:", values)
+
+appointment = st.slider(
+    "Schedule your appointment:", value=(time(11, 30), time(12, 45))
+)
+st.write("You're scheduled for:", appointment)
+
+start_time = st.slider(
+    "When do you start?",
+    value=datetime(2020, 1, 1, 9, 30),
+    format="MM/DD/YY - hh:mm",
+)
+st.write("Start time:", start_time)
+
